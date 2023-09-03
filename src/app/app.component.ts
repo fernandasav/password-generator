b59452b17320f50bb070bdda8fb089b0f07054ca
fernanda-savatin <savatinfernanda@gmail.com>
2023-09-03 20:57:29 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'password-generator';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  password='';
+  useLetters= false;
+  useNumbers= false;
+  useSymbols= false;
+  length= 0;
+
+  onChangeLength(event: Event){
+    const target = event.target as HTMLInputElement;
+    const parsedValue = parseInt(target.value);
+     
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters(){
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onButtonClick(){
+    const numbers= '0123456789';
+    const letters= 'abcdefghijklmnopqrstuvwxyz';
+    const symbols= '@#$%&*()-=+|/';
+
+    let validChars='';
+
+    if(this.useLetters){
+      validChars += letters;
+    }
+    if(this.useNumbers){
+      validChars += numbers;
+    } 
+    if(this.useSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++){
+      const index = validChars.charAt(Math.floor(Math.random()*validChars.length));
+      generatedPassword += [index];
+    } 
+    this.password = generatedPassword;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {NgFor} from '@angular/common';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule, 
+    NgFor, 
+    FormsModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
